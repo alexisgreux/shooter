e7d360746b71bc694d4da205ef5b65b17104d8c1
alexisgreux <alexisgreux@outlook.fr>
2018-11-19 14:52:24 +0100

    replace step-start by step-game + reset du son

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,29 @@ const shooter = {}
  * Set up
  */
 shooter.$container = document.querySelector('.shooter')
+shooter.$start = shooter.$container.querySelector('.start')
 shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.score = 0
 
-
 shooter.sounds = {}
 shooter.sounds.ding = new Audio('ding.mp3')
-shooter.sounds.finish = new Audio ('finish.mp3')
+shooter.sounds.finish = new Audio('finish.mp3')
+
+shooter.$start.addEventListener('click', () =>
+{
+    shooter.$container.classList.replace('step-start', 'step-game')
+})
 
 /**
  * Methods
  */
+shooter.start = () =>
+{
+    console.log('start')
+}
+
 shooter.addTarget = () =>
 {
     // Create element
@@ -44,10 +54,10 @@ shooter.shootTarget = (_$target) =>
     // Increment score
     shooter.score++
     shooter.$score.textContent = shooter.score
+    
     // Play sound
+    shooter.sounds.ding.currentTime = 0
     shooter.sounds.ding.play()
-
-
 }
 
 shooter.addTarget()
